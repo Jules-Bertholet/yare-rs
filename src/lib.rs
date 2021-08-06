8ccb3f28db43a65b0d56798eaffc17e253676d80
Jules Bertholet <jules.bertholet@gmail.com>
2021-08-06 15:30:14 +0200

    Fix graphics

--- src/lib.rs
+++ src/lib.rs
@@ -692,30 +692,73 @@ pub fn tick() -> &'static u32 {
 /// Module for Yare's built-in graphics methods.
 pub mod graphics {
     use crate::Position;
+    use js_sys::Object;
     use wasm_bindgen::prelude::*;
 
     #[wasm_bindgen]
     extern "C" {
-        #[wasm_bindgen(js_namespace = graphics, getter)]
-        pub fn style() -> String;
+        #[wasm_bindgen(extends = Object, typescript_type = "Graphics")]
+        #[derive(Clone, Debug)]
+        type Graphics;
 
-        #[wasm_bindgen(js_namespace = graphics, setter)]
-        pub fn set_style(style: &str);
+        #[wasm_bindgen(method, getter)]
+        fn style(this: &Graphics) -> String;
 
-        #[wasm_bindgen(js_namespace = graphics, getter)]
-        pub fn linewidth() -> f64;
+        #[wasm_bindgen(method, setter)]
+        fn set_style(this: &Graphics, style: &str);
 
-        #[wasm_bindgen(js_namespace = graphics, setter)]
-        pub fn set_linewidth(linewidth: f64);
+        #[wasm_bindgen(method, getter)]
+        fn linewidth(this: &Graphics) -> f64;
 
-        #[wasm_bindgen(js_namespace = graphics)]
-        pub fn line(pos: &Position, end: &Position);
+        #[wasm_bindgen(method, setter)]
+        fn set_linewidth(this: &Graphics, linewidth: f64);
 
-        #[wasm_bindgen(js_namespace = graphics)]
-        pub fn circle(pos: &Position, r: f64);
+        #[wasm_bindgen(method)]
+        fn line(this: &Graphics, pos: &Position, end: &Position);
 
-        #[wasm_bindgen(js_namespace = graphics)]
-        pub fn rect(tl: &Position, br: &Position);
+        #[wasm_bindgen(method)]
+        fn circle(this: &Graphics, pos: &Position, r: f64);
+
+        #[wasm_bindgen(method)]
+        fn rect(this: &Graphics, tl: &Position, br: &Position);
+
+        #[wasm_bindgen]
+        static graphics: Graphics;
+    }
+
+    #[inline(always)]
+    pub fn style() -> String {
+        graphics.style()
+    }
+
+    #[inline(always)]
+    pub fn set_style(style: &str) {
+        graphics.set_style(style);
+    }
+
+    #[inline(always)]
+    pub fn linewidth() -> f64 {
+        graphics.linewidth()
+    }
+
+    #[inline(always)]
+    pub fn set_linewidth(linewidth: f64) {
+        graphics.set_linewidth(linewidth);
+    }
+
+    #[inline(always)]
+    pub fn line(pos: &Position, end: &Position) {
+        graphics.line(pos, end);
+    }
+
+    #[inline(always)]
+    pub fn circle(pos: &Position, r: f64) {
+        graphics.circle(pos, r);
+    }
+
+    #[inline(always)]
+    pub fn rect(tl: &Position, br: &Position) {
+        graphics.rect(tl, br);
     }
 }
 
